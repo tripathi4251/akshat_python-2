ead2f9a3bdae9af7c2f4aba1b50488f32c3056a1
tripathi4251 <tripathi.4251@gmail.com>
2026-03-28 14:00:00 +0530

    add oops easy coding questions

---
+++ oops-practice/ques1.py
@@ -0,0 +1,15 @@
+#create a class called dog , add and __init__ method with parameters name and age and store them as properties using self..
+
+class Dog:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def bark(self):
+    print(self.name + " says Woof!")
+
+# Create an object
+d1 = Dog("Buddy", 3)
+
+# Call the bark method
+d1.bark()
