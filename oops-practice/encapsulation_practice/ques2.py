6756804e8e3ab6b1450863dd43b16f8d2fa7dbf9
tripathi4251 <tripathi.4251@gmail.com>
2026-03-31 01:08:09 +0530

    add easy encapsulation coding question

---
+++ oops-practice/encapsulation_practice/ques2.py
@@ -0,0 +1,25 @@
+#create a class ScoreBoard
+#Add an __init__ with a score parameter and store it as a private attribute
+#Add a method called get_score that returns the private score
+#Create an object s1 with a score of 0
+#Print the score of s1
+
+
+
+
+
+
+class scoreBoard:
+    def __init__(self,score,name):
+        self._score=score
+        self.name=name
+
+    def show(self):
+        print("the name is:"  + self.name)
+
+    def get_score(self):
+     return self._score
+    
+s1 =scoreBoard(0,"AKSHAT")
+print(s1.get_score())
+s1.show()
