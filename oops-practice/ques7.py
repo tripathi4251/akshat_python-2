929f0c06453e78ad4de76206b66f120f168c9a2c
tripathi4251 <tripathi.4251@gmail.com>
2026-04-02 01:12:32 +0530

    add the easy oops coding question

---
+++ oops-practice/ques7.py
@@ -0,0 +1,28 @@
+#create account class with 2 attributes balance & account no. create methods for debit credi
+#and printing the balance..
+
+class account:
+    def __init__(self,bal,acc):
+        self.balance=bal
+        self.account_no=acc
+
+
+    def debit(self, amount):
+        self.balance=-amount
+        print("rs.",amount , "was debited")
+        print("total balance =", self.get_balance())
+
+    def credit(self,amount):
+        self.balance+=amount
+        print("rs." , amount , "was credited")
+        print("total balance=" , self.get_balance())
+
+    def get_balance(self):
+        return self.balance
+    
+acc1 = account(100000 , 12345)
+print(acc1.balance)
+print(acc1.account_no)
+acc1.debit(1000)
+acc1.credit(500)
+acc1.credit(400000)
