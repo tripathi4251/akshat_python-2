d638a7f752f486d3dbf28d064411650962a2885e
tripathi4251 <tripathi.4251@gmail.com>
2026-04-13 16:22:26 +0530

    add medium inheritance coding question

---
+++ oops-practice/inheritance_practice/medium_ques2.py
@@ -0,0 +1,29 @@
+#Shape → Circle & Rectangle
+# What to do:
+#Use same method name area() but different logic (polymorphism + inheritan
+
+class shape:
+    pass   # no need of common constructor
+
+class rectangle(shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+    
+
+class circle(shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+    
+
+r = rectangle(16, 15)
+print(r.area())
+
+c = circle(4)
+print(c.area())
