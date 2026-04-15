21a13af0c33ce06d51fcc5404b8b18b515edc0cc
tripathi4251 <tripathi.4251@gmail.com>
2026-04-15 21:00:32 +0530

    add medium polymorphism coding question

---
+++ oops-practice/polymorphism_practice/medium_ques6.py
@@ -0,0 +1,19 @@
+#Duck Typing
+#Task:
+#Write a function that works for different objects without checking type.
+
+class bike:
+    def start(self):
+        print("bike start with kick")
+
+class car:
+    def start(self):
+        print("car start with keys")
+    
+def vehicle(sound):
+        sound.start()
+
+    
+    
+vehicle(bike())
+vehicle(car())
